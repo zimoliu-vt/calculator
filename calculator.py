3bfe05d1957110a97b4db8d2a96fbc73effcc707
zimoliu-vt <131325658+zimoliu-vt@users.noreply.github.com>
2023-04-20 03:11:39 +0000

    it works!

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""A simple calculator.
+Zimo Liu (2023)"""
+
+first_number = input("")
+first_number = int(first_number)
+
+second_num = input("")
+second_num = int(second_num)
+
+anser = first_number+second_num
+print(anser)
